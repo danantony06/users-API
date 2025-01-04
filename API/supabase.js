673dfb2a96429edee21ae0a8ccc65e56e86542f7
Daniel Antony <danantony06@gmail.com>
2025-01-04 16:48:51 -0600

    Worked on the Supabase Endpoints as well as fixed up the Create User Endpoint

--- API/supabase.js
+++ API/supabase.js
@@ -2,6 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 const dotenv = require("dotenv");
 dotenv.config();
 
+
 if(!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
     throw new Error("Missing Supabase URL or Anonomys Key");
 }
