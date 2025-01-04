d2d001764b381d6412d5fa760645770326d8db9b
Daniel Antony <danantony06@gmail.com>
2025-01-04 00:38:49 -0600

    Moving on from Render work, now onto Supabase

---
+++ API/supabase.js
@@ -0,0 +1,15 @@
+const { createClient } = require("@supabase/supabase-js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+if(!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+    throw new Error("Missing Supabase URL or Anonomys Key");
+}
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
+
+module.exports = supabase;
--- API/users-API.js
+++ API/users-API.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+console.log("hello")
 
 
 const pool = new Pool({
@@ -23,6 +24,7 @@ const pool = new Pool({
 
 app.get("/", (req, res) =>
 {
+    console.log("hellow ordfk")
     res.json({message: "Welcome to my database!" });
 });
 
@@ -35,6 +37,42 @@ app.get("/users", async (req, res) => {
         res.status(500).json({ error: 'Failed to retrieve users' });
       }
     });
+
+
+
+app.get("/users/:id", async (req,res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = ${req.params.id}`);
+        res.status(200).json(result.rows); 
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: req.params.id });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to retrieve user' });
+    }
+});
+
+
+
+
+
+
+
+app.post("/users/create", async (req,res) => {
+    console.log('this')
+
+    const {query} = req.body;
+    res.json({message: req.body.firstname });
+    // console.log(req.body)
+    // const result = await pool.query(`SELECT * FROM users WHERE first_name = ${req.body.firstname}`);
+    // return res.status(200).json(result.rows); 
+    // return res.status(404).json({ error: req.params.firstname });
+    // const result = await pool.query(`INSERT INTO users (first_name,last_name,email) ${req.params.firstname}`);
+    // res.json(result.rows);
+    
+})
+
 
 
   
